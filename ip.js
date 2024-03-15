c80b36bcfa32d84203111ff539a66944f74caf39
Cinol123 <147175235+Cinol123@users.noreply.github.com>
2024-03-15 19:02:07 +0100

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,9 +1,24 @@
-const requestIp = require('request-ip'); 
- 
-app.use(requestIp.mw()); 
- 
-app.get('/', function(req, res) { 
-  const clientIp = req.clientIp; 
-  console.log(`Visitor's IP address: ${clientIp}`); 
-  res.send(`Your IP address is ${clientIp}`); 
-}); 
+const fs = require('fs');
+const requestIp = require('request-ip');
+const express = require('express');
+const app = express();
+
+app.use(requestIp.mw());
+
+app.get('/', function(req, res) {
+  const clientIp = req.clientIp;
+  console.log(`Visitor's IP address: ${clientIp}`);
+  
+  // Zapisz adres IP klienta do pliku
+  fs.appendFile('ip_logs.txt', `${clientIp}\n`, (err) => {
+    if (err) {
+      console.error('Błąd podczas zapisywania adresu IP do pliku:', err);
+    }
+  });
+  
+  res.send(`Your IP address is ${clientIp}`);
+});
+
+app.listen(3000, () => {
+  console.log('Serwer nasłuchuje na porcie 3000');
+});
